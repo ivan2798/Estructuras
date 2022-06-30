009f4971b14e8111d7fac2c51bb86b44a43c4c38
ivan2798 <inge1998@hotmail.com>
2022-06-30 18:28:26 -0500

    arreglos y objetos

---
+++ script.js
@@ -0,0 +1,50 @@
+/* console.log("Bienvenidos a estructuras de datos");
+ */
+
+//ARRAYS
+
+let listaCompra = ["pan","leche","fruta","pescado"];
+//let si creo que se va a modificar
+//const se va a mantener constante
+
+let cantidades = [3,10,5,2]
+
+let registroEntradas = [true,true,false,false]
+
+//OBJETOS
+/* let cliente = {
+    nombre: "Hipermarket",
+    direccion:"LA HABAS",
+    ciudad: "MarketCity",
+    factura: 2300,
+    facturasPendientes: true
+} */
+
+//COMBINACIONES
+
+//ARREGLO DE OBJETOS
+
+let arreglo = [{},{},{}]
+
+
+//came1Case
+const topTresJugadoresTenis =[{
+    //key:value
+    ranking:1,
+    nombre:"Ivan",
+    puntos:1160,
+    pais:"Serbia"
+},{
+     //key:value
+     ranking:2,
+     nombre:"Jose",
+     puntos:9999,
+     pais:"España"
+},{
+     //key:value
+     ranking:1,
+     nombre:"Thiem",
+     puntos:4444,
+     pais:"Austria"
+}]
+
